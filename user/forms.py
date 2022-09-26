47d0ead0716aee83ecc2b2f60c1da737ce8fe106
samtechy26 <samkan061@gmail.com>
2022-09-26 17:01:29 -0400

    profile update form save achieved

--- user/forms.py
+++ user/forms.py
@@ -12,7 +12,7 @@ class UserRegistrationForm(UserCreationForm):
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ['first_name','username', 'last_name', 'email']
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
