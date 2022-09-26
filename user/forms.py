3de7b54d2511eca2a888bd52b120f79457343da9
Sam Akan <samakan061@gmail.com>
2022-09-26 23:13:20 +0300

    Made a few changes to the profile update form

--- user/forms.py
+++ user/forms.py
@@ -5,98 +5,6 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
 class UserRegistrationForm(UserCreationForm):
-    username = forms.CharField(
-        widget=forms.widgets.TextInput(
-
-            attrs={
-
-                "placeholder": "username",
-
-                "class": "form-control",
-
-            }
-
-        ),
-
-        label="username",
-    )
-    first_name = forms.CharField(
-        widget=forms.widgets.TextInput(
-
-            attrs={
-
-                "placeholder": "First name",
-
-                "class": "form-control",
-
-            }
-
-        ),
-
-        label="First name",
-
-    )
-    last_name = forms.CharField(
-        widget=forms.widgets.TextInput(
-
-            attrs={
-
-                "placeholder": "last name",
-
-                "class": "form-control",
-
-            }
-
-        ),
-
-        label="last name",
-    )
-    email = forms.EmailField(
-        widget=forms.widgets.EmailInput(
-
-            attrs={
-
-                "placeholder": "Email",
-
-                "class": "form-control",
-
-            }
-
-        ),
-
-        label="Email address",
-    )
-    password1 = forms.CharField(
-        widget=forms.widgets.PasswordInput(
-
-            attrs={
-
-                "placeholder": "Password",
-
-                "class": "form-control",
-
-            }
-
-        ),
-
-        label="password",
-    )
-    password2 = forms.CharField(
-        widget=forms.widgets.PasswordInput(
-
-            attrs={
-
-                "placeholder": "Password Confirmation",
-
-                "class": "form-control",
-
-            }
-
-        ),
-
-        label="Password Confirmation",
-    )
-    
     class Meta:
         model = User
         fields = ['username','first_name', 'last_name', 'email', 'password1', 'password2']
@@ -104,7 +12,7 @@ class UserRegistrationForm(UserCreationForm):
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email']
+        fields = ['first_name', 'last_name', 'email']
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from distutils.log import error
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
@@ -43,7 +44,7 @@ def profile(request):
 @login_required
 def profileUpdate(request):
     if request.method == 'POST':
-        u_form = UserRegistrationForm(request.POST,instance=request.user)
+        u_form = UserUpdateForm(request.POST,instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
 
         if u_form.is_valid() and p_form.is_valid():
@@ -52,8 +53,9 @@ def profileUpdate(request):
             messages.success(request, f'Your account has been updated!')
             return redirect('profile')
     else:
-        u_form = UserRegistrationForm(instance=request.user)
+        u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
+        
 
     context = {
         'u_form':u_form,
