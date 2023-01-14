4abe3deda834175eeec10896ccc41d5fdcca8e4f
Sam Akan <samakan061@gmail.com>
2023-01-14 20:07:43 +0300

    logo and favicon fixed

--- jobya/urls.py
+++ jobya/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from payment.views import stripe_webhook
 import notifications.urls
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))),
     path('admin/', admin.site.urls),
     path('stripe', stripe_webhook, name='stripe_webhook'),
     path('', include('job.urls', namespace='job')),
