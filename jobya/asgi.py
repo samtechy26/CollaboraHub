213df225763f70e6f933faba4ae9501f67da883b
Sam Akan <samakan061@gmail.com>
2023-01-14 20:35:29 +0300

    asgi trial 2 with railway.app

--- jobya/asgi.py
+++ jobya/asgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-import chat.routing
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobya.settings')
 
