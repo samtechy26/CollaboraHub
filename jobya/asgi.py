363b6154c961f61693ab1413fb0bc19ec1320a29
samtechy26 <samkan061@gmail.com>
2023-01-18 15:04:24 -0500

    deployment new

--- jobya/asgi.py
+++ jobya/asgi.py
@@ -13,16 +13,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobya.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobya.settings')
 
 
-application = get_asgi_application()
-
-# application = ProtocolTypeRouter({
-#     'http': get_asgi_application(),
-#     'websocket': AuthMiddlewareStack(
-#         URLRouter(
-#             chat.routing.websocket_urlpatterns
-#         )
-#     )
-# })
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
+})
