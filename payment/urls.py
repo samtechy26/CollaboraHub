d6bbc221fe72a9863e1528dec256df21dae07741
Sam Akan <samakan061@gmail.com>
2023-06-23 12:41:24 +0300

    changes made to payment urls

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CreateCheckoutSessionView, SuccessView
-from .views import CreateCheckoutSessionView, SuccessView, CancelView, create_coinbase_payment, coinbase_webhook
+from .views import CreateCheckoutSessionView, SuccessView, CancelView, create_coinbase_payment, coinbase_webhook, stripe_webhook
 
 urlpatterns = [ 
     path('success/', SuccessView.as_view(), name='success'),
     path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
     path('coinbase-pay/<pk>/', create_coinbase_payment, name='coinbase-payment'),
-    path('webhook/',coinbase_webhook),
+    path('webhook/', stripe_webhook, name='stripe-webhook'),
+    path('coinbasewebhook/',coinbase_webhook),
 ]
